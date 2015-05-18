656372414123d630e807379edaab18daf7b3a881
Zeljko Minic <zeljko.minic@saga.rs>
2015-05-18 15:06:01 +0200

    initial import

---
+++ WcfProxyInterceptionTests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfProxyInterceptionTests.SampleService;
+
+namespace WcfProxyInterceptionTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly IUnityContainer _container;
+
+        public UnitTest1()
+        {
+            _container = new UnityContainer();
+
+            _container.AddNewExtension<Interception>();
+
+            _container.RegisterType<IService1, Service1Client>("Basic", new InjectionConstructor());
+
+            _container.RegisterType<IService1, Service1Client>(new InjectionConstructor(),
+                new Interceptor<InterfaceInterceptor>(),
+                new InterceptionBehavior<WcfProxyInterceptor>());
+        }
+
+        [TestMethod]
+        public void WcfInterceptor_CanCreateNewProxyIfPreviousIsClosed()
+        {
+            var service = _container.Resolve<IService1>();
+
+            service.GetData(1);
+            service.GetData(2);
+        }
+
+        [TestMethod]
+        public async Task WcfInterceptor_CanCloseProxyForAsyncMethod()
+        {
+            var service = _container.Resolve<IService1>();
+
+            var result = await service.GetIntAsync();
+
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public async Task WcfInterceptor_CanCloseProxyForAsyncMethodThatReturnsError()
+        {
+            var service = _container.Resolve<IService1>();
+
+            try
+            {
+                var result = await service.GetIntExceptionAsync();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
---
+++ WcfProxyInterceptionTests/WcfProxyInterceptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace WcfProxyInterceptionTests
+{
+    public class WcfProxyInterceptor : IInterceptionBehavior
+    {
+        private readonly IUnityContainer _container;
+
+        public WcfProxyInterceptor(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            // create new instance of proxy (because previous call might have closed proxy)
+            var proxy = _container.Resolve(input.MethodBase.DeclaringType, "Basic") as ICommunicationObject;
+
+            // ensure that we got ICommunicationObject
+            if (proxy == null)
+                throw new InvalidOperationException("Interception attempted on non wcf proxy interface");
+
+            var success = false;
+
+            try
+            {
+                var args = input.Arguments.Cast<object>().ToArray();
+
+                // invoke method on new proxy
+                var result = input.MethodBase.Invoke(proxy, args);
+
+                proxy.Close();
+                success = true;
+
+                return input.CreateMethodReturn(result, args);
+            }
+            catch (Exception ex)
+            {
+                if (!success)
+                    proxy.Abort();
+
+                return input.CreateExceptionMethodReturn(ex);
+            }
+        }
+
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return new[] { typeof(ICommunicationObject) };
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+    }
+}
